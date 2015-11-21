786011ffa34dc527d78ab4671c4cc4b58f2a7b37
Ryan Hodin <ryan951357@gmail.com>
2015-11-21 17:42:41 -0600

    Added a boilerplate login box
    This box is not connected to any storage and is not yet properly styled.

---
+++ GameTindr/scripts/common.js
@@ -0,0 +1,9 @@
+function login()
+{
+	document.getElementById("headerLoginDialog").style.display="inline"
+}
+
+function submitLogin()
+{
+	document.getElementById("headerLoginDialog").style.display="none"
+}
