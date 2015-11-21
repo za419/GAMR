d37f5f09dc40ccc50d0be5bd7000deb3d70db677
Murfalo <murphycangelo@u.northwestern.edu>
2015-11-21 15:56:21 -0600

    More changes to index.php

--- GameTindr/index.php
+++ GameTindr/index.php
@@ -1,16 +1,12 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <?php include("include/common.html") ?>
     <meta charset="utf-8">
     <title>Gamr</title>
   </head>
   <body>
-    <div id="header" valign="top">
       <?php include("include/header.html"); ?>
-    <div>
-
-    <div id="footer" valign="bottom">
       <?php include("include/footer.html"); ?>
-    </div>
   </body>
 </html>
