c36fe95bdb3decc9e1c8812262c3aff5856f2cf0
Murfalo <murphycangelo@u.northwestern.edu>
2015-11-21 18:17:51 -0600

    Added things

--- GameTindr/register.php
+++ GameTindr/register.php
@@ -2,17 +2,17 @@
 <html>
   <head>
     <?php include("include/common.html"); ?>
-    <link rel="stylesheet" type="text/css" name="register.css">
+    <link rel="stylesheet" type="text/css" href="styles/register.css">
     <meta charset="utf-8">
     <title>Gamr</title>
   </head>
   <body>
       <?php include("include/header.html"); ?>
-      <form id="register-form">
-        <input name="username" placeholder="Username"><br>
-        <input name="password" placeholder="Password"><br>
-        <input name="confirm-password" placeholder="Confirm password"><br>
-        <button type="button" class="btn-sm btn-default">Submit</button>
+      <form id="registerForm">
+          <input type="text" name="username" placeholder="Username"><br>
+          <input type="password" name="password" placeholder="Password"><br>
+          <input type="password" name="confirm-password" placeholder="Confirm password"><br>
+          <input type="submit">
       </form>
       <?php include("include/footer.html"); ?>
   </body>
