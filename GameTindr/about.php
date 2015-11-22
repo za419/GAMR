52fa99b5ec6b22404f9943576c3820cbf0ab6a14
Ryan Hodin <ryan951357@gmail.com>
2015-11-21 20:36:08 -0600

    Fixed about page formatting

--- GameTindr/about.php
+++ GameTindr/about.php
@@ -10,7 +10,10 @@
       <h1 id="title">About <span class="gamr">GAMR</span></h1>
 		<br>
 		<p class="info">
-			&nbsp;&nbsp;<span class="gamr">GAMR</span> was written as a team project for the WildHacks 2015 hackathon. It was conceptualized as a way for people who wish to find a party with which to play a multi-player game to find one another and play together. As team member <span class="teammate-name">Murphy Angelo</span> said, <blockquote>I think that it's disappointing how there are a lot of cool games that people don't really play online, and that you can't really find anyone to play with locally. I wish there was an easier way to find people to play with.</blockquote>
+			&nbsp;&nbsp;<span class="gamr">GAMR</span> was written as a team project for the WildHacks 2015 hackathon. It was conceptualized as a way for people who wish to find a party with which to play a multi-player game to find one another and play together. As team member <span class="teammate-name">Murphy Angelo</span> said,
+		<p class="info">
+			<blockquote>I think that it's disappointing how there are a lot of cool games that people don't really play online, and that you can't really find anyone to play with locally. I wish there was an easier way to find people to play with.</blockquote>
+		<p class="info">
 			Fellow team members <span class="teammate-name">Elana Stettin</span> and <span class="teammate-name">Ryan Hodin</span> agreed with this sentiment, and thus <span class="gamr">GAMR</span> was born.
       <?php include("include/footer.html"); ?>
   </body>
