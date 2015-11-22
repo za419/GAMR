b9fae2cf63ccd76f0fd211bac0a532d5d3647a5a
Murfalo <murphycangelo@u.northwestern.edu>
2015-11-21 18:40:10 -0600

    Add header and footer to subpages

---
+++ GameTindr/profile.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php include("include/common.html"); ?>
+    <meta charset="utf-8">
+    <title>Profile</title>
+  </head>
+  <body>
+      <?php include("include/header.html"); ?>
+      <?php include("include/footer.html"); ?>
+  </body>
+</html>
