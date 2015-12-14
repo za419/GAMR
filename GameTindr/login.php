d747c7bc3a5ef6435c92ddb4f7d493e9799b7180
Ryan Hodin <ryan951357@gmail.com>
2015-12-14 00:19:28 -0800

    Add password hash updating This will mostly be useful if PHP gets updated and we want to continue using the default hash algorithm, or if we increase the number of rounds.

--- GameTindr/login.php
+++ GameTindr/login.php
@@ -7,19 +7,26 @@ $password = $_POST['password'];
 
 if ($username && $password) {
 	$connection=mysqli_connect($CONFIG["host"],$CONFIG["username"],$CONFIG["password"],$CONFIG["dbname"]) or die("Could not connect to the server.");
-	$query=mysqli_prepare($connection,'SELECT password FROM users where username=? or email=?');
+	$query=mysqli_prepare($connection,'SELECT password,username FROM users where username=? or email=?'); // Allow login using both username and email.
 	mysqli_stmt_bind_param($query, 'ss', $username, $username);
 	mysqli_stmt_execute($query);
 	mysqli_stmt_store_result($query);
 	$numrows=mysqli_stmt_num_rows($query);
 	if ($numrows!=0) {
 		$dbpassword="";
-		mysqli_stmt_bind_result($query, $dbpassword);
+		$dbusername="";
+		mysqli_stmt_bind_result($query, $dbpassword,$dbusername);
 		mysqli_stmt_fetch($query);
-		if (password_verify($password, $dbpassword)) {
-			$_SESSION['username']=$username;
+		if (password_verify($password, $dbpassword)) { // Login succeeded
+			$_SESSION['username']=$dbusername; // Set the username to be the one in the database: This is needed if we logged in via an email.
+			if (password_needs_rehash($dbpassword, PASSWORD_DEFAULT)) { // If the stored hash needs updating,
+				$dbpassword=password_hash($password, PASSWORD_DEFAULT);
+				mysqli_stmt_close($query);
+				$query=mysqli_stmt_prepare($connection, 'UPDATE users SET password=? WHERE username=? or email=?'); // Update it.
+				mysqli_stmt_bind_param($query, 'sss', $dbpassword, $username, $username);
+				mysqli_stmt_execute($query);
+			}
 			header("Location:index.php");
-			echo("<script>document.location.replace('index.php')</script>");
 		}
 		else
 			echo("Incorrect username or password.");
